0aadf3b90cf331a6aebb3bdbcc63b384d9aa8f03
Vivek Sankhla <61795729+vivxk@users.noreply.github.com>
2020-06-19 22:01:55 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class palindrome{
+    int n;
+    int sum=0;
+
+    int checkPal(int n){
+        if(n<=0){
+            return sum;
+        }else{
+            sum=(sum*10) + n%10;
+            return checkPal(n/10);
+        }
+    }
+
+    public static void main(String[] args ){
+    Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number ");
+        int n=sc.nextInt();
+        int tmp=n;
+
+        palindrome p=new palindrome();
+        p.checkPal(n);
+        if(p.sum==tmp){
+            System.out.println(+n+" is a Palindrome");
+        }
+        else {
+            System.out.println(+n+" is not a Palindorme");
+        }
+    }
+}
+
+
+
